be8942ffb4c10472a50f27f84135307d8aff7d22
Stanislavych <stasik.bartosh@mail.ru>
2023-06-07 15:26:33 +0300

    add IUserRegistrationService interface

---
+++ ExpenseMaster.BusinessLogic/Interfaces/IUserRegistrationService.cs
@@ -0,0 +1,10 @@
+﻿using ExpenseMaster.Common.Dto;
+using ExpenseMaster.Model.Models;
+
+namespace ExpenseMaster.BusinessLogic.Interfaces
+{
+    public interface IUserRegistrationService
+    {
+        Task<User> RegisterAsync(UserRegistrationDto userDto);
+    }
+}
