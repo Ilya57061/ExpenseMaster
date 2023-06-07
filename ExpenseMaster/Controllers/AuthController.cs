790f29a5a708221aa666bd6ce3b966bd5a92a850
Stanislavych <stasik.bartosh@mail.ru>
2023-06-07 15:45:38 +0300

    add AuthController

---
+++ ExpenseMaster/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using ExpenseMaster.BusinessLogic.Interfaces;
+using ExpenseMaster.Common.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseMaster.Controllers
+{
+    [ApiController]
+    [Route("public")]
+    [AllowAnonymous]
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        private readonly IUserRegistrationService _userRegistrationService;
+
+        public AuthController(IAuthService authService, IUserRegistrationService userRegistrationService)
+        {
+            _authService = authService;
+            _userRegistrationService = userRegistrationService;
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Get(UserLoginDto userDto)
+        {
+            try
+            {
+                var user = await _authService.AuthenticateAsync(userDto);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost("Registration")]
+        public async Task<IActionResult> Create(UserRegistrationDto user)
+        {
+            try
+            {
+                await _userRegistrationService.RegisterAsync(user);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
