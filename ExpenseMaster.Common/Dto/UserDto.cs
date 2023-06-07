8bc7a5e02127fc6ab62c021d9ff40cb869c0a30b
Stanislavych <stasik.bartosh@mail.ru>
2023-06-07 15:19:22 +0300

    add dto models

---
+++ ExpenseMaster.Common/Dto/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseMaster.Common.Dto
+{
+    public class UserDto
+    {
+        public string Login { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string AuthorizationHeader { get; set; } = string.Empty;
+    }
+}
