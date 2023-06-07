5221b4b3629e3ce5023e63eb2e88a477cb3b29c6
Stanislavych <stasik.bartosh@mail.ru>
2023-06-07 13:59:37 +0300

    add IUserRepository interface

---
+++ ExpenseMaster.BusinessLogic/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseMaster.Model;
+
+namespace ExpenseMaster.BusinessLogic.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task CreateUserAsync(User user);
+        Task<User> GetUserByIdAsync(int userId);
+        Task<User> GetUserByLoginAsync(string login);
+        Task<User> GetUserByEmailAsync(string email);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(User user);
+    }
+}
