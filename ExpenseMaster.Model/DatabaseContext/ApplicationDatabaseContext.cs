9b9a8ceadc13b979e9a0fbc8b9137138d39ab432
Stanislavcod <stas.dryk@mail.ru>
2023-06-07 12:40:54 +0300

    add income entity

---
+++ ExpenseMaster.Model/DatabaseContext/ApplicationDatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseMaster.Model.DatabaseContext
+{
+    public class ApplicationDatabaseContext : DbContext
+    {
+        public ApplicationDatabaseContext(DbContextOptions<ApplicationDatabaseContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ ExpenseMaster.Model/Models/Income.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.Reflection.Metadata;
+
+namespace ExpenseMaster.Model.Models
+{
+    public class Income
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
