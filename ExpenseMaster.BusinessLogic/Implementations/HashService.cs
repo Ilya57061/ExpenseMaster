2e9b9869545724aee34a50c9e1d2fa5a2a340982
Stanislavych <stasik.bartosh@mail.ru>
2023-06-07 13:14:39 +0300

    add ITokenService interface

--- ExpenseMaster.BusinessLogic/Implementations/HashService.cs
+++ ExpenseMaster.BusinessLogic/Implementations/HashService.cs
@@ -1,10 +1,6 @@
 ﻿using ExpenseMaster.BusinessLogic.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ExpenseMaster.BusinessLogic.Implementations
 {
---
+++ ExpenseMaster.BusinessLogic/Interfaces/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using ExpenseMaster.Model;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseMaster.BusinessLogic.Interfaces
+{
+    public interface ITokenService
+    {
+        JwtSecurityToken GenerateJwtToken(User user);
+        string GetUserIdFromToken(string token);
+        string GetToken(User user);
+    }
+}
