61707c1211c4edd3cc673c194ad64f801a46e005
Stanislavych <stasik.bartosh@mail.ru>
2023-06-07 15:21:37 +0300

    add IAuthService interface

---
+++ ExpenseMaster.BusinessLogic/Interfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using ExpenseMaster.Common.Dto;
+
+namespace ExpenseMaster.BusinessLogic.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<UserDto> AuthenticateAsync(UserLoginDto userLoginDto);
+    }
+}
