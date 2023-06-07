bf5e227047b485a46e44f007f8f9fc33d8c63ab5
Stanislavcod <stas.dryk@mail.ru>
2023-06-07 14:57:00 +0300

    add migration

--- ExpenseMaster.Model/DatabaseContext/ApplicationDatabaseContext.cs
+++ ExpenseMaster.Model/DatabaseContext/ApplicationDatabaseContext.cs
@@ -10,10 +10,6 @@ namespace ExpenseMaster.Model.DatabaseContext
         {
 
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            
-        }
         public DbSet<User> Users { get; set; }
         public DbSet<Income> Incomes { get; set; }
         public DbSet<Expense> Expenses { get; set; }
--- ExpenseMaster/Configuration/ConfigurationService.cs
+++ ExpenseMaster/Configuration/ConfigurationService.cs
@@ -1,9 +1,14 @@
-﻿namespace ExpenseMaster.Configuration
+﻿using ExpenseMaster.Model.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseMaster.Configuration
 {
     public static class ConfigurationService
     {
         public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<ApplicationDatabaseContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                opt => opt.MigrationsAssembly("ExpenseMaster")));
             services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             services.AddEndpointsApiExplorer();
