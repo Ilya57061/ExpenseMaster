a8d27eea76b3a30adb3caec02d8ea6ab61aed1e8
Stanislavych <stasik.bartosh@mail.ru>
2023-06-07 13:05:58 +0300

    add IHashService interface

---
+++ ExpenseMaster.BusinessLogic/Interfaces/IHashService.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ExpenseMaster.BusinessLogic.Interfaces
+{
+    public interface IHashService
+    {
+        void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+        bool VerifyPasswordHash(string password, out byte[] passwordSalt, byte[] passwordHash);
+    }
+}
