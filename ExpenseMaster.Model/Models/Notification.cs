56ac1e65386a7da240b81b8a06c0c669d72e7f85
Илья Д <ilyad509@gmail.com>
2023-06-09 16:58:15 +0300

    update notification model

--- ExpenseMaster.Model/Models/Notification.cs
+++ ExpenseMaster.Model/Models/Notification.cs
@@ -3,8 +3,8 @@
     public class Notification
     {
         public int Id { get; set; }
-        public string Message { get; set; }
-        public string Subject { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
         public DateTime CreatedDate { get; set; }
     }
 }
