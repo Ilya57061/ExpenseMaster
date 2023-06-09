75b5e8e7d5a645edbbbde5c3bcff1a38c9ce1569
Илья Д <ilyad509@gmail.com>
2023-06-09 14:40:46 +0300

    create a Notification model

---
+++ ExpenseMaster.Model/Models/Notification.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseMaster.Model.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
