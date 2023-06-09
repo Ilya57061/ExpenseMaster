982351f0c841bc159dcca6deb348339b8778eac7
Илья Д <ilyad509@gmail.com>
2023-06-09 16:57:37 +0300

    create an EmailService

---
+++ ExpenseMaster.BusinessLogic/Interfaces/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using ExpenseMaster.Model.Models;
+
+namespace ExpenseMaster.BusinessLogic.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string email, Notification notification);
+    }
+}
--- ExpenseMaster.Model/Models/Notification.cs
+++ ExpenseMaster.Model/Models/Notification.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Message { get; set; }
+        public string Subject { get; set; }
         public DateTime CreatedDate { get; set; }
     }
 }
