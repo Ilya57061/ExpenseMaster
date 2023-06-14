26a99dd95f9befcc91416be5eb299768a6130585
Stanislavych <stasik.bartosh@mail.ru>
2023-06-14 11:42:14 +0300

    edit UserRepository

--- ExpenseMaster.BusinessLogic/Interfaces/IUserRepository.cs
+++ ExpenseMaster.BusinessLogic/Interfaces/IUserRepository.cs
@@ -4,5 +4,6 @@ namespace ExpenseMaster.BusinessLogic.Interfaces
 {
     public interface IUserRepository : IRepositoryBase<User>
     {
+        Task<User> GetUserByLoginAsync(string login);
     }
 }
