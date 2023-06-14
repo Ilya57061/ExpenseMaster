c2b82a589c2c2e1802b4901e7ab764b50453855c
Илья Д <ilyad509@gmail.com>
2023-06-14 12:32:19 +0300

    create BudgetRepository

---
+++ ExpenseMaster.BusinessLogic/Repository/BudgetRepository.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseMaster.BusinessLogic.Interfaces;
+using ExpenseMaster.Model.DatabaseContext;
+using ExpenseMaster.Model.Models;
+
+namespace ExpenseMaster.BusinessLogic.Repository
+{
+    public class BudgetRepository : RepositoryBase<Budget>, IBudgetRepository
+    {
+        public BudgetRepository(ApplicationDatabaseContext appContext) 
+            : base(appContext)
+        {
+        }
+    }
+}
