16e815f58bfd8992e800db0e10b78cfa7923fead
Илья Д <ilyad509@gmail.com>
2023-06-15 14:45:30 +0300

    create a IBudgetService

---
+++ ExpenseMaster.BusinessLogic/Interfaces/IBudgetService.cs
@@ -0,0 +1,17 @@
+﻿using ExpenseMaster.DAL.Models;
+
+namespace ExpenseMaster.BusinessLogic.Interfaces
+{
+    public interface IBudgetService
+    {
+        Task<Budget> GetByCategoryIdAsync(int userId, int categoryId);
+        Task<Budget> GetByIdAsync(int id);
+        Task CreateAsync(Budget budget);
+        Task UpdateAsync(Budget budget);
+        Task DeleteAsync(Budget budget);
+        Task<IEnumerable<Budget>> GetByUserIdAsync(int userId);
+        Task UpdateWarningThresholdAsync(int budgetId, decimal warningThreshold);
+        Task<IEnumerable<Budget>> GetBudgetsExceedingThresholdAsync(int userId);
+        Task<decimal> GetBudgetRemainingAmountAsync(int userId);
+    }
+}
