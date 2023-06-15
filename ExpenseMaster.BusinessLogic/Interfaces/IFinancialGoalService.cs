dbce3e3ba127f393ff8e16333819da0f5c492d84
Илья Д <ilyad509@gmail.com>
2023-06-15 16:37:15 +0300

    create a IFinancialGoalService

---
+++ ExpenseMaster.BusinessLogic/Interfaces/IFinancialGoalService.cs
@@ -0,0 +1,16 @@
+﻿using ExpenseMaster.DAL.Models;
+
+namespace ExpenseMaster.BusinessLogic.Interfaces
+{
+    public interface IFinancialGoalService
+    {
+        Task<FinancialGoal> GetByIdAsync(int id);
+        Task CreateAsync(FinancialGoal financialGoal);
+        Task UpdateAsync(FinancialGoal financialGoal);
+        Task DeleteAsync(int id);
+        Task<IEnumerable<FinancialGoal>> GetByUserIdAsync(int userId);
+        Task UpdateCurrentAmountAsync(int goalId, decimal currentAmount);
+        Task<IEnumerable<FinancialGoal>> GetByTargetAmountAsync();
+        Task<decimal> GetTotalProgressAsync(int userId);
+    }
+}
