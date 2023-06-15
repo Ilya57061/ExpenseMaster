78875907e2e5de05d72be2ba4175ea0cd0637070
Илья Д <ilyad509@gmail.com>
2023-06-15 16:33:21 +0300

    create a FinancialGoalRepository

---
+++ ExpenseMaster.DAL/Interfaces/IFinancialGoalRepository.cs
@@ -0,0 +1,10 @@
+﻿using ExpenseMaster.DAL.Models;
+
+namespace ExpenseMaster.DAL.Interfaces
+{
+    public interface IFinancialGoalRepository : IRepositoryBase<FinancialGoal>
+    {
+        Task<FinancialGoal> GetByIdAsync(int id);
+        Task<IEnumerable<FinancialGoal>> GetByUserIdAsync(int userId);
+    }
+}
