74f6ade3f28112a2d5f088c453571338822d6f8a
Илья Д <ilyad509@gmail.com>
2023-06-22 10:38:00 +0300

    add empty line

--- ExpenseMaster.Tests/Repository/BudgetRepositoryTests.cs
+++ ExpenseMaster.Tests/Repository/BudgetRepositoryTests.cs
@@ -101,6 +101,7 @@ namespace ExpenseMaster.Tests.Repository
             mockDbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryableData.Expression);
             mockDbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryableData.ElementType);
             mockDbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(queryableData.GetEnumerator());
+
             return mockDbSet;
         }
     }
